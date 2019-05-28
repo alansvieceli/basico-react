5916044e63120439de23ed28a1036a97c7f470b4
Alan Vieceli <alansvieceli@gmail.com>
2019-05-27 21:25:01 -0300

    retornar multiplos elementos adjacentes, ao invs de usar div.
    Usar Reac.fragment ou usar um array, separados por virgula

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import PrimeiroComponenteArrow from './Componentes/PrimeiroComponenteArrow'
-import {CompA, CompB} from './Componentes/DoisComponentes'
+///import {CompA, CompB} from './Componentes/DoisComponentes'
+import MultiElementos from './Componentes/MultiElementos';
 
 const elemento = document.getElementById('root');
 ReactDOM.render(
     <div>
-        {/* <PrimeiroComponenteArrow valor="XyZ" /> */}
+        {/* 
+        <PrimeiroComponenteArrow valor="XyZ" /> 
         <CompA valor="A" />
         <CompA valor="B" />
+        */}
+        <MultiElementos />
     </div>,
     elemento);
 
