318576b2a2054d184db96271431012c0d5837def
Alan Vieceli <alansvieceli@gmail.com>
2019-05-27 21:16:12 -0300

    Dois compoentes em um mesmo arquivo

--- src/Componentes/PrimeiroComponenteArrow.jsx
+++ src/Componentes/PrimeiroComponenteArrow.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default (props) => (
   <div>
-    <h1>{props.valor}</h1>;
+    <h1>{props.valor}</h1>
   </div>
 )
     
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PrimeiroComponenteArrow from './Componentes/PrimeiroComponenteArrow'
+//import PrimeiroComponenteArrow from './Componentes/PrimeiroComponenteArrow'
+import {CompA, CompB} from './Componentes/DoisComponentes'
 
 const elemento = document.getElementById('root');
 ReactDOM.render(
     <div>
-        <PrimeiroComponenteArrow valor="XyZ"/>
-    </div>, 
+        {/* <PrimeiroComponenteArrow valor="XyZ" /> */}
+        <CompA valor="A" />
+        <CompA valor="B" />
+    </div>,
     elemento);
 
