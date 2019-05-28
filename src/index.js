de9ba1f28952615d9eae335dfb77574e93ff03f3
Alan Vieceli <alansvieceli@gmail.com>
2019-05-27 21:03:49 -0300

    Projeto iniciado

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PrimeiroComponenteArrow from './Componentes/PrimeiroComponenteArrow'
+
+const elemento = document.getElementById('root');
+ReactDOM.render(
+    <div>
+        <PrimeiroComponenteArrow valor="XyZ"/>
+    </div>, 
+    elemento);
+
