3fb2acaa321bd4eb2bf11432b4a10647bf83898b
Alan Vieceli <alansvieceli@gmail.com>
2019-05-27 21:51:14 -0300

    Relação entre componentes #2

--- src/index.js
+++ src/index.js
@@ -6,23 +6,30 @@ import ReactDOM from 'react-dom';
 //import CompA from './Componentes/DoisComponentes'
 //import CompA, {CompB as B} from './Componentes/DoisComponentes'
 
-import FamiliaSilva from './Componentes/FamiliaSilva'
-
 //import MultiElementos from './Componentes/MultiElementos';
+//import FamiliaSilva from './Componentes/FamiliaSilva'
+
+import Familia from './Componentes/Familia'
+import Membro from './Componentes/Membro'
 
 const elemento = document.getElementById('root');
 ReactDOM.render(
     <div>
         {/* 
-        <PrimeiroComponenteArrow valor="XyZ" /> 
-        
+        <PrimeiroComponenteArrow valor="XyZ" />         
         <CompA valor="A" />
-        <B valor="B" />        
-        
-        <MultiElementos />
-        */}
-
+        <B valor="B" />                
+        <MultiElementos />        
         <FamiliaSilva />
+        */}
+        <Familia>
+            <Membro nome="Alan" sobrenome="Vieceli" />
+            <Membro nome="Jaque" sobrenome="Vieceli" />
+        </Familia>
+        <Familia>
+            <Membro nome="Rosa" sobrenome="Silva" />
+            <Membro nome="Graziela" sobrenome="Silva" />
+        </Familia>
     </div>,
     elemento);
 
