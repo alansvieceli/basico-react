fdbf64e60f4ef01f489674596b100fbf6a922509
Alan Vieceli <alansvieceli@gmail.com>
2019-05-27 21:34:00 -0300

    formas de export e import

--- src/Componentes/DoisComponentes.jsx
+++ src/Componentes/DoisComponentes.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
-const CompA = props => (
+export const CompA = props => (
     <h1>Primeiro diz: {props.valor}</h1>
 )
 
-const CompB = props => (
-    <h1>Segundo diz: {props.valor}</h1>
+export const CompB = props => (
+    <h2>Segundo diz: {props.valor}</h2>
 )
 
-export {CompA, CompB}
+//export {CompA, CompB}
+
+export default CompA;
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import PrimeiroComponenteArrow from './Componentes/PrimeiroComponenteArrow'
-///import {CompA, CompB} from './Componentes/DoisComponentes'
-import MultiElementos from './Componentes/MultiElementos';
+
+//import {CompA, CompB} from './Componentes/DoisComponentes'
+//import CompA from './Componentes/DoisComponentes'
+import CompA, {CompB as B} from './Componentes/DoisComponentes'
+
+//import MultiElementos from './Componentes/MultiElementos';
 
 const elemento = document.getElementById('root');
 ReactDOM.render(
     <div>
         {/* 
         <PrimeiroComponenteArrow valor="XyZ" /> 
-        <CompA valor="A" />
-        <CompA valor="B" />
         */}
+        <CompA valor="A" />
+        <B valor="B" />        
+        {/*
         <MultiElementos />
+        */}
     </div>,
     elemento);
 
